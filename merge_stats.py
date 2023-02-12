14a8766eb072cd01d0756e0fde1ef4744ddd8407
startler03 <jakub.stetina2003@gmail.com>
2023-02-12 14:30:05 +0100

    merge_stats script

---
+++ merge_stats.py
@@ -0,0 +1,94 @@
+############################################
+# Title:  merge_stats.py                   #
+# Author: Jakub Štětina                    #
+# Date:   12 Feb 2023                      #
+############################################
+
+import argparse
+import time
+import csv
+import sys
+import os
+
+csv.field_size_limit(sys.maxsize)
+io_parser = argparse.ArgumentParser()
+
+io_parser.add_argument(
+    "-bl","--backlinks", 
+    type = str, 
+    required=True,
+    action="store",
+    dest="bl_file",
+    help = "Input file",
+)
+
+
+io_parser.add_argument(
+    "-pw","--pageviews", 
+    type = str, 
+    required=True,
+    action="store",
+    dest="pw_file",
+    help = "Input file",
+)
+
+io_parser.add_argument(
+    "-pr","--primary-tags", 
+    type = str, 
+    required=True,
+    action="store",
+    dest="pr_file",
+    help = "Input file",
+)
+
+io_parser.add_argument(
+    "-o","--output", 
+    type = str, 
+    required=True,
+    action="store",
+    dest="out_file",
+    help = "Merged output file",
+)
+
+
+args = io_parser.parse_args()
+
+bl_file = args.bl_file
+pw_file = args.pw_file
+pr_file = args.pr_file
+out_file = args.out_file
+
+
+# OUTPUT FILE FORMAT
+# ARTICLE_NAME \t BACKLINKS \t PAGEVIEWS \t PRIMARY
+ 
+out_data = {}
+
+print("Starting")
+with open(bl_file) as bl_in, open(pw_file) as pw_in, open(pr_file) as pr_in:
+    
+    # Files in order of output format
+    in_files_list = [bl_in, pw_in, pr_in]
+
+    for idx, file in enumerate(in_files_list, start=0):
+        in_data = csv.reader(file, delimiter="\t")
+        for val in in_data:
+            art_name = val[0]
+            try:
+                count = int(val[1])
+            except ValueError:
+                continue
+
+            if art_name not in out_data:
+                out_data[art_name] = [0, 0, 0]
+            out_data[art_name][idx] = count
+
+print("Saving data..")
+with open(out_file, "w") as file_out:
+    for key, values in out_data.items():
+        file_out.write(key)
+        for value in values:
+            file_out.write(f"\t{value}")
+        file_out.write("\n")
+
+print("Finished.")
