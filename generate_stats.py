9952437ed7d969c51ef72c086b44b5b62f1bc558
startler03 <jakub.stetina2003@gmail.com>
2023-06-19 11:15:47 +0200

    Changed default stats dir to '/mnt/minerva1/nlp-in/wikipedia-statistics/stats/'

--- generate_stats.py
+++ generate_stats.py
@@ -20,7 +20,7 @@ from generate_pageviews import PageViews
 csv.field_size_limit(sys.maxsize)
 
 TMP_DIR = "unmerged"
-STATS_DIR = "stats"
+STATS_DIR = "/mnt/minerva1/nlp-in/wikipedia-statistics/stats"
 
 if not os.path.exists(TMP_DIR):
     os.mkdir(TMP_DIR)
