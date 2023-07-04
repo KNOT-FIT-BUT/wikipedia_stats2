393018c1bfe76e9708c351015874f82eee089b4c
startler03 <jakub.stetina2003@gmail.com>
2023-07-04 15:33:44 +0200

    remove_redirects function

--- generate_backlinks.py
+++ generate_backlinks.py
@@ -6,7 +6,6 @@
 
 import argparse
 import logging
-import sys
 import os
 import re
 
@@ -23,7 +22,6 @@ class Backlinks():
     # Temporary data storage
     BL_DATA = dict()
     
-
     # Set all class attributes
     def __init__(
         self,
@@ -54,6 +52,46 @@ class Backlinks():
                 out_file.write(f"{key}\t{value}\n")
         self.BL_DATA.clear()
 
+    # Goes through file and removes redirect pages 
+    # from BL_DATA and adds the backlinks to the actual page
+    def remove_redirects(self):
+        REDIRECT_TO_REG = r"<redirect(?:.*)title=\"(.*)\"(?:.*)/>"
+        REDIRECT_FROM_REG = r"<title>(.*)</title>"
+
+        with open(self.INPUT_FILE) as dump_file:
+            in_page = False
+            page_content = ""
+            redirects_num = 0
+
+            for line in dump_file:
+                
+                # Get page tag content
+                if line.strip() == "<page>":
+                    in_page = True
+                    continue    
+                
+                if line.strip() == "</page>":
+                    in_page = False
+                    redirect_to = re.search(REDIRECT_TO_REG, page_content, re.MULTILINE)
+                    redirect_from = re.search(REDIRECT_FROM_REG, page_content, re.MULTILINE)
+
+                    if redirect_to and redirect_from:
+                        redirect_to = redirect_to.group(1).replace(" ", "_")
+                        redirect_from = redirect_from.group(1).replace(" ", "_")
+
+                        if self.BL_DATA.get(redirect_from):
+                            if not self.BL_DATA.get(redirect_to):
+                                self.BL_DATA[redirect_to] = 0
+                            self.BL_DATA[redirect_to] += self.BL_DATA.pop(redirect_from)
+                            redirects_num += 1
+
+                    page_content = ""
+                    continue
+
+                if in_page:
+                    page_content += line
+        return redirects_num
+  
     # Generates backlinks from a given input dump file
     # (backlinks must match the search pattern)
     # Returns the number of generated values
@@ -70,12 +108,15 @@ class Backlinks():
                     else:
                         self.BL_DATA[a_name] += 1
                     val_counter += 1
+        
+        logging.info("Removing redirects..")
+        redirects_num = self.remove_redirects()
+        logging.info(f"Removed {redirects_num} redirects.")
         self.__save_to_file()
         logging.info("Generation complete.")
         logging.info(f"Generated {val_counter} values.")
         return val_counter
 
-# If run from CLI
 if __name__ == "__main__":
     # Argument parsing
     io_parser = argparse.ArgumentParser()
